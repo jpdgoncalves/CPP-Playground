7744afcc3775018aed9c6d52d345fe2303a624b3
Luis Ferreira <apenaspara1998@gmail.com>
2019-10-03 11:46:21 +0100

    First practice

---
+++ Multiple Files/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "add.h"
+
+int main() {
+    int result = add(2,3);
+
+    std::cout << result << "\n";
+}
---
+++ definepp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+#define LENGTH 10
+#define WIDTH 5
+
+int main() {
+
+    cout << LENGTH * WIDTH << endl;
+
+    return 0;
+}
