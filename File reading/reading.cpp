c45c6415e97d28cf0936dbc84d15b8bd51ac3cf5
Luis Ferreira <apenaspara1998@gmail.com>
2019-10-08 16:00:58 +0100

    First prototype of fcm

--- File reading/reading.cpp
+++ File reading/reading.cpp
@@ -1,30 +1,73 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include <map>
+#include <algorithm>
+
+const char ALPHABET[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','.'};
+const int BF_SIZE = 3;
+const int ALPHABET_SIZE = 28;
 
 int main() {
 
-    
+    std::map<std::string,std::vector<int>>  sequences;
     std::ifstream ifs;
     std::string key;
     char c;
-    int size = 3;
-    char bf[size];
+    char bf[BF_SIZE];
 
     ifs.open("example.txt", std::ifstream::in);
-    ifs.get(bf,size+1);
-    key.assign(bf,size);
 
-    //std::cout << key << '\n';
+    if(!ifs) {
+        std::cout << "Couldn't open file\n";
+        return -1;
+    }
+
+    ifs.get(bf,BF_SIZE+1);
+    key.assign(bf,BF_SIZE);
+
+    std::for_each(key.begin(), key.end(), [](char & ch) {
+        ch = ::tolower(ch);
+    });
+
+    std::cout << "First key: " << key << '\n';
 
     while(ifs.get(c)) {
-        std::cout << key << '-' << c << '\n';
+        
+        std::vector<int>& counts = sequences[key];
+
+        if(counts.size() == 0) {
+            counts.resize(ALPHABET_SIZE,0);
+        }
+
+        auto it = std::find( std::begin(ALPHABET), std::end(ALPHABET), char( std::tolower(c)));
+
+        if(it == std::end(ALPHABET)) {
+            std::cout << "No position found for " << c << "\n";
+            continue;
+        }
+
+        int index = std::distance( std::begin(ALPHABET), it);
+        counts[index]++;
+
+        std::cout << key << '-' << c << " index: " << index << '\n';
+
         key.erase(0,1);
         key += c;
     }
 
-    //std::cout << '\n';
+    for(auto const& pair : sequences) {
+        std::cout << pair.first << ":";
+
+        for(const int count : pair.second) {
+            std::cout << count << ",";
+        }
+
+        std::cout << "\n";
+    }
+
+    std::cout << '\n';
     ifs.close();
 
     return 0;
